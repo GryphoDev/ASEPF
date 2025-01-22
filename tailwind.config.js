2fb235c91590c57fdb509f2b43f46fd603f9a676
GryphoDev <wmauroux@icloud.com>
2025-01-22 16:00:06 +0100

    feat: add google maps on contact section and content on collectiveHousing.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./about.html", "./assets/**/*.js"],
+  content: [
+    "./index.html",
+    "./about.html",
+    "./samad.html",
+    "./job.html",
+    "./collectiveHousing.html",
+    "./diversifiedAccomodation.html",
+    "./assets/**/*.js",
+  ],
   theme: {
     extend: {
       screens: {
@@ -8,6 +16,8 @@ module.exports = {
       },
       backgroundImage: {
         banner: "url('../images/artem-kniaz-DqgMHzeio7g-unsplash.jpg')",
+        bannerCollectivePage:
+          "url('../images/marlis-trio-akbar-eMB60hNHFL8-unsplash.jpg')",
       },
     },
   },

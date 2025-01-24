9ed04b0475041b55d76d3d3c152b22b0a31f2fec
GryphoDev <wmauroux@icloud.com>
2025-01-24 01:39:27 +0100

    feat: add burger menu button animation

--- assets/js/burgerMenu.js
+++ assets/js/burgerMenu.js
@@ -1,7 +1,14 @@
 const menu = document.getElementById("menu");
 const burgerBtn = document.getElementById("burger-menu");
+const burgerAnim = document.querySelector(".burgerAnim");
 
 burgerBtn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
   menu.classList.toggle("flex");
+  burgerBtn.classList.toggle("active");
+  if (!burgerBtn.classList.contains("active")) {
+    burgerBtn.classList.add("unActive");
+  } else {
+    burgerBtn.classList.remove("unActive");
+  }
 });
